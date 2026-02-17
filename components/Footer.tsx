34c457729a1ff2d1653ec1af2ec57b095acc186a
Dumbhag <s6519410057.sau.ac.th>
2026-02-17 14:06:58 +0700

    save1

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <hr className='mt-8 mb-5 mx-auto w-full' />
+        <h3 className='text-center text-gray-500'>
+            Create by 6519410054 May IoT SAU
+        </h3>
+    </div>
+  )
+}
